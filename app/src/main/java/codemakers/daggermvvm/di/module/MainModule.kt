59d2e3a871efdc2036ec2da8ded40ef48bb1ada5
Felipe Arcos <efarcos@unicauca.edu.co>
2017-10-19 15:31:52 -0500

    activity_main creado

--- app/src/main/java/codemakers/daggermvvm/di/module/MainModule.kt
+++ app/src/main/java/codemakers/daggermvvm/di/module/MainModule.kt
@@ -1,7 +1,11 @@
 package codemakers.daggermvvm.di.module
 
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
 import codemakers.daggermvvm.di.ActivityScope
 import codemakers.daggermvvm.ui.adapters.TodoAdapter
+import codemakers.daggermvvm.ui.main.MainActivity
+import codemakers.daggermvvm.ui.main.MainViewModel
 import dagger.Module
 import dagger.Provides
 
@@ -11,6 +15,11 @@ import dagger.Provides
 
 @Module
 class MainModule {
+
+    @ActivityScope
+    @Provides
+    fun provideViewModel(activity: MainActivity, factory: ViewModelProvider.Factory): MainViewModel
+            = ViewModelProviders.of(activity, factory).get(MainViewModel::class.java)
 
     @ActivityScope
     @Provides
